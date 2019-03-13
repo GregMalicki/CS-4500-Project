2319a225d042e6a173deb5667f9f44153872e5dc
GregMalicki <gregory-malicki@uiowa.edu>
2019-03-13 18:18:33 -0500

    added task model

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import timedelta
+from django.utils import timezone
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    last_completed = models.DateTimeField(default=timezone.now)
+    frequency = models.IntegerField(default=45)
+
+    def complete(self):
+        self.last_completed = timezone.now
+
+    def time_remaining(self):
+        time = timezone.now() - self.last_completed
+        return divmod(time.total_seconds(), 60)[0]
