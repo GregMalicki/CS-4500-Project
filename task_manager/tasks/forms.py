c7bc4fed618c3c4bab6352db04a672a5176a6adf
GregMalicki <gregory-malicki@uiowa.edu>
2019-03-13 19:13:50 -0500

    added ability to create a new task

---
+++ task_manager/tasks/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+
+
+class NewTaskForm(forms.Form):
+
+    new_name = forms.CharField(
+        max_length=50,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'newName',
+                'placeholder': 'New Task Name'
+            }
+        ))
+    new_rate = forms.CharField(
+        max_length=50,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'newRate',
+                'placeholder': ''
+            }
+        ))
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from tasks.forms import NewTaskForm
+from tasks.models import Task
 
 
 def home(request):
-    return render(request, 'homepage.html')
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            Task.objects.create(name=cd['new_name'], frequency=cd['new_rate'])
+            return render(request, 'homepage.html', {'form': NewTaskForm(), 'tasks': Task.objects.all()})
+        return render(request, 'homepage.html', {'form': NewTaskForm(), 'tasks': Task.objects.all()})
+    if request.method == 'GET':
+        return render(request, 'homepage.html', {'form': NewTaskForm(), 'tasks': Task.objects.all()})
+
+
+
+
